b3926b59a61116c004fa6c86185d2b8110fbbb80
Alexander Orlov <aleksandr.orlov@sputnix.ru>
2022-07-21 19:57:54 +0300

    Some refacrotring.

--- main/ws_echo_server.cpp
+++ main/ws_echo_server.cpp
@@ -24,37 +24,29 @@
 
 #include "single_include/nlohmann/json.hpp"
 
-struct async_resp_arg {
-    httpd_handle_t hd;
-    int fd;
-};
+void async_sender(void*);
+esp_err_t my_ws_handler(httpd_req_t *req);
+esp_err_t my_handler (httpd_req_t *req);
 
 static const char *TAG = "ws_echo_server";
 httpd_handle_t server = NULL;
+TaskHandle_t async_sender_handler;
 
-static void ws_async_send(void *arg)
-{
-    static const char * data = "Async data";
-    struct async_resp_arg *resp_arg = reinterpret_cast<struct async_resp_arg*> (arg);
-    httpd_handle_t hd = resp_arg->hd;
-    int fd = resp_arg->fd;
-    httpd_ws_frame_t ws_pkt;
-    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
-    ws_pkt.payload = (uint8_t*)data;
-    ws_pkt.len = strlen(data);
-    ws_pkt.type = HTTPD_WS_TYPE_TEXT;
+static const httpd_uri_t ws = {
+        .uri        = "/ws",
+        .method     = HTTP_GET,
+        .handler    = my_ws_handler,
+        .user_ctx   = NULL,
+        .is_websocket = true
+};
 
-    httpd_ws_send_frame_async(hd, fd, &ws_pkt);
-    free(resp_arg);
-}
-
-static esp_err_t trigger_async_send(httpd_handle_t handle, httpd_req_t *req)
-{
-    struct async_resp_arg* resp_arg = reinterpret_cast<struct async_resp_arg*> (malloc(sizeof(struct async_resp_arg)));
-    resp_arg->hd = req->handle;
-    resp_arg->fd = httpd_req_to_sockfd(req);
-    return httpd_queue_work(handle, ws_async_send, resp_arg);
-}
+static const httpd_uri_t my_handler_header = {
+        .uri        = "/",
+        .method     = HTTP_GET,
+        .handler    = my_handler,
+        .user_ctx   = NULL,
+        .is_websocket = true
+};
 
 esp_err_t my_handler (httpd_req_t *req)
 {
@@ -65,68 +57,25 @@ esp_err_t my_handler (httpd_req_t *req)
     uint32_t  ws_test_html_size = ws_test_html_end - ws_test_html_start;
 
     httpd_resp_send_chunk(req, (const char *)ws_test_html_start, ws_test_html_size);
-
     httpd_resp_sendstr_chunk(req, NULL);
     return ret;
 }
 
-/*
- * This handler echos back the received ws data
- * and triggers an async send if certain message received
- */
-static esp_err_t echo_handler(httpd_req_t *req)
+esp_err_t my_ws_handler(httpd_req_t *req)
 {
     if (req->method == HTTP_GET) {
-        ESP_LOGI(TAG, "Handshake done, the new connection was opened");
-        return ESP_OK;
-    }
-    httpd_ws_frame_t ws_pkt;
-    uint8_t *buf = NULL;
-    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
-    ws_pkt.type = HTTPD_WS_TYPE_TEXT;
-    /* Set max_len = 0 to get the frame len */
-    esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 0);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "httpd_ws_recv_frame failed to get frame len with %d", ret);
-        return ret;
-    }
-    ESP_LOGI(TAG, "frame len is %d", ws_pkt.len);
-    if (ws_pkt.len) {
-        /* ws_pkt.len + 1 is for NULL termination as we are expecting a string */
-        buf = reinterpret_cast<uint8_t*> (calloc(1, ws_pkt.len + 1));
-        if (buf == NULL) {
-            ESP_LOGE(TAG, "Failed to calloc memory for buf");
-            return ESP_ERR_NO_MEM;
+        size_t size = 8;
+        int* sockets = new int[size];
+        if(httpd_get_client_list(server, &size, sockets) == ESP_OK)
+        {
+            ESP_LOGI(TAG, "Handshake done, the new connection was opened, number of clients: %u", size);
         }
-        ws_pkt.payload = buf;
-        /* Set max_len = ws_pkt.len to get the frame payload */
-        ret = httpd_ws_recv_frame(req, &ws_pkt, ws_pkt.len);
-        if (ret != ESP_OK) {
-            ESP_LOGE(TAG, "httpd_ws_recv_frame failed with %d", ret);
-            free(buf);
-            return ret;
+        else
+        {
+            ESP_LOGE(TAG, "Somthing got wrong, number of clients: %u", size);
         }
-        ESP_LOGI(TAG, "Got packet with message: %s", ws_pkt.payload);
-    }
-    ESP_LOGI(TAG, "Packet type: %d", ws_pkt.type);
-    if (ws_pkt.type == HTTPD_WS_TYPE_TEXT &&
-        strcmp((char*)ws_pkt.payload,"Trigger async") == 0) {
-        free(buf);
-        return trigger_async_send(req->handle, req);
-    }
+        delete[] sockets;
 
-    ret = httpd_ws_send_frame(req, &ws_pkt);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "httpd_ws_send_frame failed with %d", ret);
-    }
-    free(buf);
-    return ret;
-}
-
-static esp_err_t my_ws_handler(httpd_req_t *req)
-{
-    if (req->method == HTTP_GET) {
-        ESP_LOGI(TAG, "Handshake done, the new connection was opened");
         return ESP_OK;
     }
 
@@ -162,7 +111,7 @@ static esp_err_t my_ws_handler(httpd_req_t *req)
     if (ws_pkt.type == HTTPD_WS_TYPE_TEXT &&
         strcmp((char*)ws_pkt.payload,"Trigger async") == 0) {
         free(buf);
-        return trigger_async_send(req->handle, req);
+        return ESP_OK;
     }
 
     ret = httpd_ws_send_frame(req, &ws_pkt);
@@ -172,22 +121,6 @@ static esp_err_t my_ws_handler(httpd_req_t *req)
     free(buf);
     return ret;
 }
-
-static const httpd_uri_t ws = {
-        .uri        = "/ws",
-        .method     = HTTP_GET,
-        .handler    = my_ws_handler,
-        .user_ctx   = NULL,
-        .is_websocket = true
-};
-
-static const httpd_uri_t my_handler_header = {
-        .uri        = "/",
-        .method     = HTTP_GET,
-        .handler    = my_handler,
-        .user_ctx   = NULL,
-        .is_websocket = true
-};
 
 static httpd_handle_t start_webserver(void)
 {
@@ -200,9 +133,9 @@ static httpd_handle_t start_webserver(void)
         // Registering the ws handler
         ESP_LOGI(TAG, "Registering URI handlers");
         httpd_register_uri_handler(server, &ws);
-
         httpd_register_uri_handler(server, &my_handler_header);
 
+        xTaskCreate(async_sender, "async_sender", 4096, NULL, configMAX_PRIORITIES-5, &async_sender_handler);
         return server;
     }
 
@@ -213,6 +146,7 @@ static httpd_handle_t start_webserver(void)
 static void stop_webserver(httpd_handle_t server)
 {
     // Stop the httpd server
+    vTaskDelete(&async_sender_handler);
     httpd_stop(server);
 }
 
@@ -259,7 +193,7 @@ void async_sender(void*)
                     using namespace std;
                     json test;
                     test["uptime"] = uptime_s;
-                    test["translation"] = { sin(uptime_s) * 100.0f , 0.0f, -500.0f};
+                    test["translation"] = {sin(uptime_s) * 100.0f , 0.0f, -500.0f};
 
                     string data = test.dump();
                     uint32_t len = data.size();
@@ -312,6 +246,5 @@ void app_main(void)
     /* Start the server for the first time */
     server = start_webserver();
 
-    xTaskCreate(async_sender, "async_sender", 4096, NULL, configMAX_PRIORITIES-5, NULL);
 }
 }
